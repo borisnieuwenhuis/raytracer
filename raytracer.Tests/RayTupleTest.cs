7494b4f3554e8883feebe25d351cb2ccd58b6950
Boris Nieuwenhuis <boris@Boriss-MBP.home>
2023-09-06 10:44:32 +0200

    WIP

--- raytracer.Tests/RayTupleTest.cs
+++ raytracer.Tests/RayTupleTest.cs
@@ -9,10 +9,22 @@ namespace raytracer.Tests
             RayTuple point= RayTuple.createPoint(1, 2, 3);
             RayTuple vector = RayTuple.createVector(1, 2, 3);
             RayTuple result = point + vector;
-            Assert.Equal(7, result.x);
-            Assert.Equal(8, result.y);
-            Assert.Equal(10, result.z);
-            Assert.Equal(12, tuple3.w);
+            Assert.Equal(2, result.X);
+            Assert.Equal(4, result.Y);
+            Assert.Equal(6, result.Z);
+            Assert.Equal(RayTuple.Vector, result.W);
+        }
+
+        [Fact]
+        public void TestSubtract()
+        {
+            RayTuple point1 = RayTuple.createPoint(3, 2, 1);
+            RayTuple point2 = RayTuple.createPoint(5, 6, 7);
+            RayTuple result = point1 - point2;
+            Assert.Equal(-2, result.X);
+            Assert.Equal(-4, result.Y);
+            Assert.Equal(-6, result.Z);
+            Assert.Equal(RayTuple.Vector, result.W);
         }
     }
 }
--- raytracer/RayTuple.cs
+++ raytracer/RayTuple.cs
@@ -3,27 +3,48 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace raytracer
 {
-	public struct RayTuple
-	{
+    public struct RayTuple
+    {
         public const int Point = 1;
-        public const int Vector= 0;
+        public const int Vector = 0;
 
         private readonly int x;
         private readonly int y;
         private readonly int z;
         private readonly int w;
 
+        public readonly int X { get { return x; } }
+        public readonly int Y { get { return y; } }
+        public readonly int Z { get { return z; } }
+        public readonly int W { get { return w; } }
+
         public static RayTuple createPoint(int x, int y, int z)
-		{
-			return new RayTuple(Point, x, y, z);
-		}
+        {
+            return new RayTuple(Point, x, y, z);
+        }
 
         public static RayTuple createVector(int x, int y, int z)
         {
             return new RayTuple(Vector, x, y, z);
         }
 
+        public static bool isPoint(RayTuple rayTuple)
+        {
+            return rayTuple.W == RayTuple.Point;
+        }
+
+        public static bool isVector(RayTuple rayTuple)
+        {
+            return rayTuple.W == RayTuple.Vector;
+        }
+
         public static RayTuple operator +(RayTuple a, RayTuple b) => new RayTuple(Vector, a.x + b.x, a.y + b.y, a.z + b.z);
+
+        public static RayTuple operator -(RayTuple a, RayTuple b) {
+
+            int resultType = isPoint(a) && isPoint(b) || isVector(a) && isVector(b) ? RayTuple.Vector : RayTuple.Point;
+            return new RayTuple(resultType, a.x - b.x, a.y - b.y, a.z - b.z);
+        }
 
         public RayTuple(int w, int x, int y, int z)
 		{
