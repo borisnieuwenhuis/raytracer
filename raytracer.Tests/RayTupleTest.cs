00a53357915f4aa35df7155f97004654d1c4dedd
Boris Nieuwenhuis <boris@Boriss-MBP.home>
2023-09-06 12:54:08 +0200

    WIP

--- raytracer.Tests/RayTupleTest.cs
+++ raytracer.Tests/RayTupleTest.cs
@@ -16,7 +16,7 @@ namespace raytracer.Tests
         }
 
         [Fact]
-        public void TestSubtract()
+        public void TestSubtractPoints()
         {
             RayTuple point1 = RayTuple.createPoint(3, 2, 1);
             RayTuple point2 = RayTuple.createPoint(5, 6, 7);
@@ -25,6 +25,58 @@ namespace raytracer.Tests
             Assert.Equal(-4, result.Y);
             Assert.Equal(-6, result.Z);
             Assert.Equal(RayTuple.Vector, result.W);
+        }
+
+        [Fact]
+        public void TestSubtractVectorFromPoint()
+        {
+            RayTuple point = RayTuple.createPoint(3, 2, 1);
+            RayTuple vector = RayTuple.createVector(5, 6, 7);
+            RayTuple result = point - vector;
+            Assert.Equal(-2, result.X);
+            Assert.Equal(-4, result.Y);
+            Assert.Equal(-6, result.Z);
+            Assert.Equal(RayTuple.Point, result.W);
+        }
+
+        [Fact]
+        public void TestSubtractVectorFromVector()
+        {
+            RayTuple vector1 = RayTuple.createVector(3, 2, 1);
+            RayTuple vector2 = RayTuple.createVector(5, 6, 7);
+            RayTuple result = vector1 - vector2;
+            Assert.Equal(-2, result.X);
+            Assert.Equal(-4, result.Y);
+            Assert.Equal(-6, result.Z);
+            Assert.Equal(RayTuple.Vector, result.W);
+        }
+
+        [Fact]
+        public void TestNegateTuple()
+        {
+            RayTuple vector1 = RayTuple.createVector(3, 2, 1);
+            RayTuple result = -vector1;
+            Assert.Equal(-3, result.X);
+            Assert.Equal(-2, result.Y);
+            Assert.Equal(-1, result.Z);
+            Assert.Equal(RayTuple.NegPoint, result.W);
+        }
+
+        [Fact]
+        public void TestMult()
+        {
+            RayTuple point1 = RayTuple.createPoint(3, 2, 1);
+            RayTuple result = 3 * point1;
+            Assert.Equal(9, result.X);
+            Assert.Equal(6, result.Y);
+            Assert.Equal(3, result.Z);
+            Assert.Equal(3, result.W);
+
+            result = 0.5 * point1;
+            Assert.Equal(1.5, result.X);
+            Assert.Equal(1, result.Y);
+            Assert.Equal(0.5, result.Z);
+            Assert.Equal(0.5, result.W);
         }
     }
 }
--- raytracer/RayTuple.cs
+++ raytracer/RayTuple.cs
@@ -6,24 +6,25 @@ namespace raytracer
     public struct RayTuple
     {
         public const int Point = 1;
+        public const int NegPoint = -1;
         public const int Vector = 0;
 
-        private readonly int x;
-        private readonly int y;
-        private readonly int z;
-        private readonly int w;
+        private readonly double x;
+        private readonly double y;
+        private readonly double z;
+        private readonly double w;
 
-        public readonly int X { get { return x; } }
-        public readonly int Y { get { return y; } }
-        public readonly int Z { get { return z; } }
-        public readonly int W { get { return w; } }
+        public readonly double X { get { return x; } }
+        public readonly double Y { get { return y; } }
+        public readonly double Z { get { return z; } }
+        public readonly double W { get { return w; } }
 
-        public static RayTuple createPoint(int x, int y, int z)
+        public static RayTuple createPoint(double x, double y, double z)
         {
             return new RayTuple(Point, x, y, z);
         }
 
-        public static RayTuple createVector(int x, int y, int z)
+        public static RayTuple createVector(double x, double y, double z)
         {
             return new RayTuple(Vector, x, y, z);
         }
@@ -40,13 +41,18 @@ namespace raytracer
 
         public static RayTuple operator +(RayTuple a, RayTuple b) => new RayTuple(Vector, a.x + b.x, a.y + b.y, a.z + b.z);
 
+        public static RayTuple operator *(double b, RayTuple a) => new RayTuple(a.w * b, a.x * b, a.y * b, a.z * b);
+
         public static RayTuple operator -(RayTuple a, RayTuple b) {
 
             int resultType = isPoint(a) && isPoint(b) || isVector(a) && isVector(b) ? RayTuple.Vector : RayTuple.Point;
             return new RayTuple(resultType, a.x - b.x, a.y - b.y, a.z - b.z);
         }
 
-        public RayTuple(int w, int x, int y, int z)
+        public static RayTuple operator -(RayTuple a) => new RayTuple(NegPoint, -1 * a.x, -1 * a.y, -1 * a.z);
+        
+
+        public RayTuple(double w, double x, double y, double z)
 		{
 			this.w = w;
 			this.x = x;
