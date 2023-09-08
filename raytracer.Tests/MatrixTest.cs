2a79801f176235845a17675d3aaf91e7b4001957
Boris Nieuwenhuis <borisnieuwenhuis@gmail.com>
2023-09-08 17:01:17 +0200

    added matrix multiplication

--- raytracer.Tests/MatrixTest.cs
+++ raytracer.Tests/MatrixTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+
 namespace raytracer.Tests
 {
 	public class MatrixTest
@@ -50,6 +52,44 @@ namespace raytracer.Tests
             m2.Set(1, 1, -2);
 
             Assert.NotEqual(m1, m2);
+        }
+
+        private void fillMatrix(Matrix m, double[] values)
+        {
+            for (int i = 0; i < values.GetLength(0); i++)
+            {
+                double value = values[i];
+                int row = (int)Math.Floor((double)i / 4);
+                int column = i % 4;
+                m.Set(row, column, value);
+            }
+        }
+
+        [Fact]
+        public void testMult()
+        {
+            double[] m1Values = {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2
+            };
+
+            Matrix m1 = new Matrix(4, 4);
+            fillMatrix(m1, m1Values);
+
+            double[] m2Values = {
+                -2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8 
+            };
+
+            Matrix m2 = new Matrix(4, 4);
+            fillMatrix(m2, m2Values);
+
+            double[] m3Values = {
+                20, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42
+            };
+
+            Matrix m3 = new Matrix(4, 4);
+            fillMatrix(m3, m3Values);
+
+            Assert.Equal(m3, m1 * m2);
         }
     }
 }
--- raytracer/Matrix.cs
+++ raytracer/Matrix.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace raytracer
 {
-	public class Matrix
-	{
+    public class Matrix
+    {
         private double[,] values;
 
         public Matrix(long x, long y)
-		{
+        {
             values = new double[x, y];
         }
 
@@ -24,6 +25,50 @@ namespace raytracer
 
         public static bool operator ==(Matrix left, Matrix right) => left.Equals(right);
         public static bool operator !=(Matrix left, Matrix right) => !(left == right);
+        public static Matrix operator *(Matrix left, Matrix right) {
+            Matrix m = new Matrix(4, 4);
+            for (int i = 0; i < m.values.GetLongLength(0); i++)
+            {
+                for (int j = 0; j < m.values.GetLongLength(1); j++)
+                {
+                    double[] row = left.Row(i);
+                    double[] column = right.Column(j);
+                    var pairs = row.Zip(column, (row, column) => row * column);
+                    m.values[i, j] = pairs.Sum();
+                }
+            }
+            return m;
+        }
+
+        public override string ToString()
+        {
+
+            List<String> rows = new List<string>();
+            for (int i = 0; i < values.GetLongLength(0); i++)
+            {
+                List<String> row = new List<string>();
+                for (int j = 0; j < values.GetLongLength(1); j++)
+                {
+                    row.Add(values[i, j].ToString());
+                }
+                rows.Add(String.Join("|", row));
+            }
+            return String.Join("\n", rows);
+        }
+
+        public double[] Row(int i)
+        {
+            return Enumerable.Range(0, values.GetLength(0))
+                .Select(x => values[i, x])
+                .ToArray();
+        }
+
+        public double[] Column(int i)
+        {
+            return Enumerable.Range(0, values.GetLength(1))
+                .Select(x => values[x, i])
+                .ToArray();
+        }
 
         public override bool Equals(object? obj)
         {
